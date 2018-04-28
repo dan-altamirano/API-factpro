862eb3961ea1c606eadffbc991f236b09383162e
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-04-28 18:16:12 -0500

    Se agreó Nodemon, node_modules, y de hicieron pruebas de Get users/user

--- factpro.js
+++ factpro.js
@@ -3,4 +3,15 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hola prro!'))
 
+// Get all users
+app.get('/users', (req, res) => {
+  res.send('You are in users section')
+})
+
+// Get one user
+app.get('/users/:userId', (req, res) => {
+  res.send('This is the profile for user: ' + req.params.userId)
+})
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
